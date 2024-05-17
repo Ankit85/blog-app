d2fec374fc1e9d77fa94efbacc9a5c65b034141f
Ankit <ankit60.av@gmail.com>
2024-05-17 13:56:27 +0530

    redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+import authService from "./appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((user) => {
+        if (user) {
+          //dispatch login
+          dispatch(login({ user }));
+        } else {
+          //dispatch logout
+          dispatch(logout());
+        }
+      })
+      .catch((err) =>
+        console.log("Something went wrong while fetaching user", err)
+      )
+      .finally(() => setIsLoading(false));
+  }, [dispatch]);
+
+  if (isLoading) return <>Loading....</>;
   return (
     <>
       <h1>The Production ready blog app</h1>
