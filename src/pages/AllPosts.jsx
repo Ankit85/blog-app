cd23cc7a9fb8c0fa81db7be15f01c7124a441014
Ankit <ankit60.av@gmail.com>
2024-05-20 19:33:15 +0530

    pages added

---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import services from "../appwrite/config";
+import PostCard from "../components";
+
+function AllPosts() {
+  const [posts, setPosts] = useState([]);
+  services.getPosts().then((allPosts) => setPosts(allPosts));
+  return (
+    <div>
+      {posts?.map((post) => (
+        <div key={post.$id}>
+          <PostCard {...post} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AllPosts;
---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { PostForm } from "../components";
+import { useNavigate, useParams } from "react-router-dom";
+import services from "../appwrite/config";
+
+function EditPostPage() {
+  const [post, setPost] = useState();
+  const navigate = useNavigate();
+  const { slug } = useParams();
+
+  useEffect(() => {
+    if (slug) {
+      services.getPost(slug).then((postItem) => {
+        if (postItem) setPost(postItem);
+      });
+    } else {
+      navigate("/");
+    }
+  }, [navigate, slug]);
+
+  if (!post) return;
+
+  return (
+    <div>
+      <PostForm {...post} />
+    </div>
+  );
+}
+
+export default EditPostPage;
